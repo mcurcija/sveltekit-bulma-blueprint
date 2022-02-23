ca47a125dc2d2eecf3a8ba8e00ac95d764d81c2c
Mario Curcija <mario.curcija@gmail.com>
2022-02-23 07:47:25 +0100

    bulma adder applied, minimal ui changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container section">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+	<h1 class="title">Hello World</h1>
+	<p class="subtitle">
+		My first website with <strong>Bulma</strong>!
+	</p>
+</div>
