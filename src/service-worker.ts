2080a70e3b107558cb559261fe7fb03fb3f92180
Mario Curcija <mario.curcija@gmail.com>
2022-03-26 09:34:07 +0100

    fixed manifest.json and service-workers

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,64 +1,68 @@
 /**
- * A copy from 
+ * This service-worker file is copy from: 
  * https://github.com/NicholasPeretti/service-worker-ts-example/blob/master/src/index.ts
- * with with timestamp instead of version 
+ *  
  */
 
- import { timestamp, build } from '$service-worker';
-
-
+/**
+ * Fix the self reference in order to have
+ * service worker intellisense.
+ * More info here:
+ * https://github.com/Microsoft/TypeScript/issues/11781#issuecomment-503773748
+ */
 declare var self: ServiceWorkerGlobalScope;
-export { };
+export {};
 
-const CACHE_NAME = `cache-${timestamp}`;
+import { version } from '../package.json';
 
-const filesToCache = ["/"];
+const CACHE_NAME = `static-cache-${version}`;
+const filesToCache = ['/'];
 
 /**
  * Cache files on install
  */
-self.addEventListener("install", event => {
-    event.waitUntil(
-        (async function () {
-            const cache = await caches.open(CACHE_NAME);
-            await cache.addAll(filesToCache);
-        })()
-    );
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		(async function () {
+			const cache = await caches.open(CACHE_NAME);
+			await cache.addAll(filesToCache);
+		})()
+	);
 });
 
 /**
  * Delete outdated caches when activated
  */
-self.addEventListener("activate", event => {
-    self.clients.claim();
+self.addEventListener('activate', (event) => {
+	self.clients.claim();
 
-    event.waitUntil(
-        (async function () {
-            // Remove old caches.
-            const promises = (await caches.keys()).map(cacheName => {
-                if (CACHE_NAME !== cacheName) {
-                    return caches.delete(cacheName);
-                }
-            });
+	event.waitUntil(
+		(async function () {
+			// Remove old caches.
+			const promises = (await caches.keys()).map((cacheName) => {
+				if (CACHE_NAME !== cacheName) {
+					return caches.delete(cacheName);
+				}
+			});
 
-            await Promise.all<any>(promises);
-        })()
-    );
+			await Promise.all<any>(promises);
+		})()
+	);
 });
 
 /**
  * Reply with cached data when possible
  */
-self.addEventListener("fetch", event => {
-    if (event.request.method !== "GET") {
-        return;
-    }
-    event.respondWith(
-        (async function () {
-            const cachedResponse = await caches.match(event.request, {
-                ignoreSearch: true
-            });
-            return cachedResponse || fetch(event.request);
-        })()
-    );
+self.addEventListener('fetch', (event) => {
+	if (event.request.method !== 'GET') {
+		return;
+	}
+	event.respondWith(
+		(async function () {
+			const cachedResponse = await caches.match(event.request, {
+				ignoreSearch: true
+			});
+			return cachedResponse || fetch(event.request);
+		})()
+	);
 });
